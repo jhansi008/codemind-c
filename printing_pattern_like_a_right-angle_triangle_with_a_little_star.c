cb2be3961cf688b5b80e266d7f348f1df3c5a821
jhansi008 <147475139+jhansi008@users.noreply.github.com>
2023-12-04 20:53:44 +0530

    #include <stdio.h> int main() {     int n,i,j;     scanf("%d",&n);     for(j=1;j<=n;j++)     {         for(i=n;i>=1;i--)         {             if(i<=j)             {                 printf("*");             }         }         printf(" ");     } }

---
+++ printing_pattern_like_a_right-angle_triangle_with_a_little_star.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+    int n,i,j;
+    scanf("%d",&n);
+    for(j=1;j<=n;j++)
+    {
+        for(i=n;i>=1;i--)
+        {
+            if(i<=j)
+            {
+                printf("*");
+            }
+        }
+        printf("
+");
+    }
+}
