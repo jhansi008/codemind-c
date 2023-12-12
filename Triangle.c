4f09148e00c78af9ca636d87386c2a5ae67d2981
jhansi008 <147475139+jhansi008@users.noreply.github.com>
2023-12-12 22:22:33 +0530

    #include <stdio.h> int main() {     int a,b,c;     scanf("%d%d%d",&a,&b,&c);     if(a==b&&a==c&&b==c)     printf("Equilateral triangle");     else if(a==b||a==c||b==c)     printf("Isosceles triangle");     else     printf("Scalene triangle"); }

--- Triangle.c
+++ Triangle.c
@@ -1,18 +1,12 @@
 #include <stdio.h>
 int main()
 {
-    int s1,s2,s3;
-    scanf("%d%d%d",&s1,&s2,&s3);
-    if(s1==s2&&s1==s3&&s2==s3)
-    {
-        printf("Equilateral triangle");
-    }
-    else if(s1==s2||s1==s3||s2==s3)
-    {
-        printf("Isosceles triangle");
-    }
-    else
-    {
-        printf("Scalene triangle");
-    }
+    int a,b,c;
+    scanf("%d%d%d",&a,&b,&c);
+    if(a==b&&a==c&&b==c)
+    printf("Equilateral triangle");
+    else if(a==b||a==c||b==c)
+    printf("Isosceles triangle");
+    else 
+    printf("Scalene triangle");
 }
