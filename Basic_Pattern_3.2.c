269a89a41ae93ae43549612b9bf719ad81deb97b
jhansi008 <147475139+jhansi008@users.noreply.github.com>
2023-12-17 11:18:58 +0530

    #include <stdio.h> int main() {     int n,i,j;     scanf("%d",&n);     for(j=1;j<=n;j++)     {         for(i=n;i>=1;i--)         {             if(j<=i)             printf("%d ",j);         }     } }

---
+++ Basic_Pattern_3.2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    int n,i,j;
+    scanf("%d",&n);
+    for(j=1;j<=n;j++)
+    {
+        for(i=n;i>=1;i--)
+        {
+            if(j<=i)
+            printf("%d ",j);
+        }
+    }    
+}
